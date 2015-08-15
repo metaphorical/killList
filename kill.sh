d6e9aa909e01b89fd1dc62e5b45333fdd4a0e7bf
Rastko Vukasinovic <rastko@vast.com>
2015-08-15 16:50:36 +0200

    selectable list and select item method, still some kinks to pass

--- kill.sh
+++ kill.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+
+UP=$'\033[A'
+DOWN=$'\033[B'
+
+
 #
 # Log <type> <msg>
 #
 log() {
-  printf "  \033[36m%10s\033[0m : \033[90m%s\033[0m\n" $1 $2
+  printf "  \033[36m%10s\033[0m : \033[0m%s\033[0m\n" $1 $2
 }
 
 #
@@ -11,6 +16,27 @@ log() {
 #
 abort() {
   printf "\n  \033[31mError: $@\033[0m\n\n" && exit 1
+}
+
+
+fullscreen_on() {
+  tput smcup
+  stty -echo
+}
+
+fullscreen_off() {
+  tput rmcup
+  stty echo
+}
+
+handle_sigint() {
+  leave_fullscreen
+  exit $?
+}
+
+handle_sigtstp() {
+  leave_fullscreen
+  kill -s SIGSTOP $$
 }
 
 #
@@ -32,7 +58,7 @@ show_cursor() {
 #
 
 process_list() {
-  ps aux | grep $1 | awk '{printf "%s--%s--%s\n", $1, $2, $12}'
+  ps aux | grep $1 | awk '{printf "[%s]%s--%s\n", $2, $1, $12}'
 }
 
 #
@@ -42,15 +68,69 @@ display_process_with_selected() {
   processList=$(process_list $1)
   i=0
   echo
+  log "KillList" "$1"
   for process in $processList; do
-    if test $i -eq 0; then
-      printf "  \033[36mo\033[0m $process\033[0m\n"
+    if test $i -eq $2; then
+      printf "  \033[36mo <$((i+1))>\033[0m $process\033[0m\n"
     else
-      printf "   \033[90m $process\033[0m\n"
+      printf "    <$((i+1))>\033[90m $process\033[0m\n"
     fi
     i=$((i+1))
   done
+  printf "   \033[90m Kill all\033[0m\n"
+  printf "   \033[90m exit\033[0m\n"
   echo
 }
 
-display_process_with_selected $1
+#
+# Do the list item action
+#
+
+#@TODO - kill # process, kill all processes or exit
+activate_list_item() {
+    clear
+    printf "Process List\n"
+    printf "$processList\n"
+    printf "position\n"
+    printf "$1\n"
+#    fullscreen_off
+}
+
+#
+# Display select list of processes and options for process of name $1
+#
+
+display_list() {
+    fullscreen_on
+    position = 0
+    display_process_with_selected $1 0
+
+    trap handle_sigint INT
+    trap handle_sigtstp SIGTSTP
+#@TODO - add scrolling limits - make it able to select kill all and exit
+    while true; do
+        read -n 3 c
+        case "$c" in
+          $UP)
+            clear
+            position=$((position-1))
+            display_process_with_selected $1 $position
+            ;;
+          $DOWN)
+            clear
+            position=$((position+1))
+            display_process_with_selected $1 $position
+            ;;
+          *)
+            activate_list_item $position
+#            fullscreen_off
+            exit
+            ;;
+        esac
+      done
+
+}
+
+#display_process_with_selected $1 $2
+
+display_list $1
